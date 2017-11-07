df01f266be95cf48ef947a41b6f2200b8cff85b4
MonkeyKing2016 <1584747392@qq.com>
2017-11-07 10:21:09 +0800

    增加junit单元测试基类

---
+++ mk-share-order-service/src/test/java/com/mk/share/order/service/base/TestJunitBaseConfig.java
@@ -0,0 +1,14 @@
+package com.mk.share.order.service.base;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author wanghao
+ * @create 2017-11-07 10:18
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:applicationContext-test.xml"})
+public class TestJunitBaseConfig {
+}
---
+++ mk-share-order-service/src/test/java/com/mk/share/order/service/impl/TestMain.java
@@ -0,0 +1,18 @@
+package com.mk.share.order.service.impl;
+
+import com.mk.share.order.service.base.TestJunitBaseConfig;
+import org.junit.Test;
+
+/**
+ * test main
+ *
+ * @author wanghao
+ * @create 2017-11-07 10:19
+ */
+public class TestMain extends TestJunitBaseConfig {
+
+    @Test
+    public void test01(){
+        System.out.println("hello tomcat");
+    }
+}
